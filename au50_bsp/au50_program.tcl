61f9f8a6426e6a4693e1e1926f5c20b49a9f6bd0
wnew <{ID}+{username}@users.noreply.github.com>
2022-07-11 12:00:23 +0200

    updates to the programming script and the readme

--- au50_bsp/au50_program.tcl
+++ au50_bsp/au50_program.tcl
@@ -1,20 +1,30 @@
 open_hw_manager
 connect_hw_server -allow_non_jtag
 open_hw_target
-current_hw_device [get_hw_devices xcu50_u55n_0]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcu50_u55n_0] 0]
-create_hw_cfgmem -hw_device [lindex [get_hw_devices xcu50_u55n_0] 0] [lindex [get_cfgmem_parts {mt25qu01g-spi-x1_x2_x4}] 0]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.FILES [list "/home/kat/au50_bsp.mcs" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
-refresh_hw_device [lindex [get_hw_devices xcu50_u55n_0] 0]
+# set the correct name for the device you are targeting.
+# this can be found via the hardware manager.
+# xcu50_u55n_0, xcu50_u55n_0_1, xcu50_u55n_0_2 etc
+set au50_dev xcu50_u55n_0
+current_hw_device [get_hw_devices $au50_dev]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices $au50_dev] 0]
+
+# add the flash memory device to the jtag
+create_hw_cfgmem -hw_device [lindex [get_hw_devices $au50_dev] 0] [lindex [get_cfgmem_parts {mt25qu01g-spi-x1_x2_x4}] 0]
+
+# configure the parameters for the mcs file programming
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.FILES [list "./au50_bsp.mcs" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
+refresh_hw_device [lindex [get_hw_devices $au50_dev] 0]
+
+# program the flash device with the mcs file
 startgroup 
-create_hw_bitstream -hw_device [lindex [get_hw_devices xcu50_u55n_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xcu50_u55n_0] 0]]; program_hw_devices [lindex [get_hw_devices xcu50_u55n_0] 0]; refresh_hw_device [lindex [get_hw_devices xcu50_u55n_0] 0];
-program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcu50_u55n_0] 0]]
+create_hw_bitstream -hw_device [lindex [get_hw_devices $au50_dev] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices $au50_dev] 0]]; program_hw_devices [lindex [get_hw_devices $au50_dev] 0]; refresh_hw_device [lindex [get_hw_devices $au50_dev] 0];
+program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $au50_dev] 0]]
 endgroup
--- readme.md
+++ readme.md
@@ -11,15 +11,22 @@ This design also serves as the infrastructure into which [Matlab/Simulink toolfl
 
 Please ensure that you have at Vivado 2021.1 installed and the correct board files for your hardware, otherwise the project generation will fail. You will need to sign up with Xilinx for access to the Alveo Vivado files before being able to download them.
 
-AU50 - board files v1.3
-AU280 - board files v1.1
+| Board | Version         |
+|-------|-----------------|
+|AU50   | board files v1.3|
+|AU280  | board files v1.1|
 
 [Alveo board files](https://www.xilinx.com/member/alveo-vivado.html)
 
 
 ## Project and Bitfile Generation
 
-Change directory into the Alveo card folder you would like to target. eg au50_bsp
+Clone this repo and change into the directory of the card you'd like to target eg au50_bsp
+
+```bash
+git clone https://github.com/ska-sa/alveo_bsp.git
+cd alveo_bsp/au50_bsp
+```
 
 Source the settings64.sh script in your Vivado install directory (edit the following command):
 
@@ -34,7 +41,7 @@ vivado
 
 In the tcl command-line run:
 ```bash
-"source au50_bsp.tcl"
+source au50_bsp.tcl
 ```
 
 Alternatively you can generate the project and bitfiles from the command-line with the following command:
@@ -42,12 +49,33 @@ Alternatively you can generate the project and bitfiles from the command-line wi
 vivado -mode batch -source au50_bsp.tcl
 ```
 
-## Programming
+This will output a bitfile and an MCS flash image file. The bitstream can be programmed to the SDRAM via JTAG and tested if required. The MCS file is the one required to program the flash.
 
-Open the Vivado hardware manager and open the target.
+## Flash Programming
 
+The programming of the flash requires a JTAG programmer, please refer to the Alveo documentation for the guide to setting this hardware up.
 
-## Misc Notes
+If you have multiple Alveos in the host machine, you will need to update the programming tcl script to point to the correct card. You can get your Alveos name via the hardware manager by opening the target device. The AU50s are named xcu50_u55n_0, xcu50_u55n_0_1, xcu50_u55n_0_2 etc
+
+Start Vivado ensuring that the au50_bsp.mcs file is in the directory vivado is started from.
+
+In the Vivado tcl command-line run:
+```bash
+source au50_program.tcl
+```
+
+This will take a few minutes to erase and prgram the flash. Once done the host machine will require a cold reboot to ensure the PCIe bus is powered down and the new flash imaged is loaded on bootup. 
+
+Once rebooted run:
+```bash
+lspci -d 10ee: -vvv -D
+```
+to check that the card had booted with the new flash image.
+
+Registers can be read to determine the version of the image. 
+ToDo: update readme with these registers and values.
+
+## Misc Notes (ToDo: put these in some sort of structure)
 
 The flash on the AU50 consists of a golden image at 0x0 and the flash images from 0x01002000. When the mcs file is uploaded it will need to be at this address. The golden image is write protected and is fallen back to if the flash image fails to load.
 
